40aaf8c27d814d913e01954a0b51a1e0defe972a
Ryan Bent <ryanbent01@gmail.com>
2015-08-11 21:08:29 +0000

    fix params error

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -4,7 +4,21 @@ class LeadsController < ApplicationController
     @leads = Lead.all
   end
 
+  def new
+    @lead = Lead.new
+  end
 
-  
+  def create
+    @lead = Lead.new(lead_params)
+binding.pry
+
+  end
+
+
+  private
+
+  def lead_params
+    params.require(:name).permit(:name,:email, :phone_number, :website, :company, :company_type)
+  end
 
 end
