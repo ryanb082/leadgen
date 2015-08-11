06e0458a57aaa7b02a172793ada0846b74d48d31
Ryan Bent <ryanbent01@gmail.com>
2015-08-11 17:48:54 +0000

    Install bootstrap

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -1,6 +1,12 @@
 class LeadsController < ApplicationController
 
   def index
+    @leads = Lead.all
+  end
+
+
+  def lead_params
+    params.require(:lead).permit(:name, :email, :phone_number, :website, :company, :company_type)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'leads#index'
 
+  resources :leads
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
