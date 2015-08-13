6912790ae9c1ee7923c4b3e24a82f3f006297353
Ryan Bent <ryanbent01@gmail.com>
2015-08-13 21:16:10 +0000

    fix leads showing in users area

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,15 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+    session[:user_id] = @user.id
       flash[:notice] = "You are registered"
       redirect_to leads_path
     else
       render :new
-    end
+    end    
+  end
 
+  def show
     
   end
 
